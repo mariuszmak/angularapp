3420c894732510e5878982af4cee5d3c1f2af1f3
mariuszmak <39630886+mariuszmak@users.noreply.github.com>
2018-12-22 15:04:44 -0500

    Moving Project
    Moving Project

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Customer } from './Customer';
+
+@Component({
+    selector: 'sandbox',
+    templateUrl: `./sandbox.component.html`,
+    styleUrls: ['./sandbox.component.css']
+})
+
+export class SandboxComponent {
+    name = 'John Doe';
+    age = 35;
+    person = {firstname: 'Steve', lastname: 'Smith"'}
+    myNumberArray:number[] =[1,2,3];
+    customer:Customer;
+
+    people = ['Rick', 'Carl', 'Derek'];
+
+    constructor() {
+        console.log('Constructor run...');
+        this.age = 36;
+        console.log('Constructor run...' + this.myNumberArray);
+        this.customer = {
+            id: 23,
+            name: 'Mia',
+            email: 'email'
+        };
+        console.log('Customer...' + this.customer);
+    }
+}
+
